e3393db5f4062964d525bac0f4657844260b62cb
Yuji Iwai <iwai@sonicmoov.com>
2016-02-27 20:09:18 +0900

    bugfix: optimized files permission denied

--- docker-hub-register.sh
+++ docker-hub-register.sh
@@ -5,7 +5,7 @@ VERSION=4.4
 
 cd $(dirname $0)/${VERSION}
 
-docker build -t ${REPOSITORY}:${VERSION} .
+docker build --no-cache -t ${REPOSITORY}:${VERSION} .
 
 docker tag $(docker images -q ${REPOSITORY}:${VERSION}) ${REPOSITORY}:4
 docker tag $(docker images -q ${REPOSITORY}:${VERSION}) ${REPOSITORY}:latest
